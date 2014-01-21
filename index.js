f3b2188f26b5e88ecb621a017e53812b92e70535
ertoth <ertoth@paypal.com>
2014-01-21 13:15:05 -0800

    impl and coverage

---
+++ index.js
@@ -0,0 +1,90 @@
+'use strict';
+
+
+var dustjs = require('dustjs-linkedin');
+
+
+var RESERVED = '☃';
+var active = 0;
+
+function patch(load, options) {
+    return function cabbage(name, chunk, context) {
+        var view, cached;
+
+        view = name;
+        cached = !!dustjs.cache[view];
+
+        if (!cached) {
+
+            view = RESERVED;
+
+            Object.defineProperty(dustjs.cache, view, {
+
+                configurable: true,
+
+                get: function () {
+                    var self;
+
+                    // Remove the getter immediately (must delete as it's a
+                    // getter. setting it to undefined will fail.)
+                    self = this;
+                    delete this[view];
+                    active += 1;
+
+                    return function faux(chunks, context) {
+
+                        function onchunk(chunk) {
+                            var args;
+
+                            function onloaded(err, src) {
+                                active -= 1;
+
+                                if (err) {
+                                    chunk.setError(err);
+                                    return;
+                                }
+
+                                if (!self[name]) {
+                                    dustjs.loadSource(dustjs.compile(src, name));
+                                }
+
+                                self[name](chunk, context).end();
+
+                                if (!options.cache) {
+                                    delete self[name];
+                                }
+                            }
+
+                            args = [name, onloaded];
+                            if (dustjs.onLoad.length === 3) {
+                                args.splice(1, 0, context);
+                            }
+
+                            dustjs.onLoad.apply(null, args);
+                        }
+
+                        return chunks.map(onchunk);
+                    };
+                }
+
+            });
+
+        }
+
+        return load(view, chunk, context);
+    };
+}
+
+
+module.exports = function contextualize(options) {
+    var load;
+
+    options = options || { cache: true };
+
+    load = dustjs.load;
+    dustjs.load = patch(load, options);
+
+    return function undo() {
+        dustjs.load = load;
+    };
+};
---
+++ test/context.js
@@ -0,0 +1,122 @@
+'use strict';
+
+
+var test = require('tape'),
+    dust = require('dustjs-linkedin'),
+    contextify = require('../');
+
+
+test('dustjs-onload-context', function (t) {
+
+    t.test('original', function (t) {
+
+        t.plan(4);
+
+        dust.onLoad = function (name, cb) {
+            t.equals(name, 'index');
+            cb(null, 'Hello, {name}!');
+        };
+
+        dust.render('index', { name: 'world' }, function (err, data) {
+            t.error(err);
+            t.equal(data, 'Hello, world!');
+            t.equal(typeof dust.cache.index, 'function');
+
+            dust.cache = {};
+
+            t.end();
+        });
+
+    });
+
+
+    t.test('with context', function (t) {
+        var undo = contextify();
+
+        t.plan(9);
+
+        dust.onLoad = function (name, context, cb) {
+            t.equals(name, 'index');
+            t.equals(typeof context, 'object');
+            t.equals(context.get('name'), 'world');
+            t.equals(typeof cb, 'function');
+            cb(null, 'Hello, {name}!');
+        };
+
+        dust.render('index', { name: 'world' }, function (err, data) {
+            t.error(err);
+            t.equal(data, 'Hello, world!');
+            t.equal(typeof dust.cache.index, 'function');
+            t.equal(dust.load.name, 'cabbage');
+
+            dust.cache = {};
+            undo();
+
+            t.equal(dust.load.name, '');
+            t.end();
+        });
+
+    });
+
+
+    t.test('primed cache', function (t) {
+        var undo = contextify();
+
+        t.plan(9);
+
+        dust.onLoad = function (name, context, cb) {
+            t.equals(name, 'index');
+            t.equals(typeof context, 'object');
+            t.equals(context.get('name'), 'world');
+            t.equals(typeof cb, 'function');
+
+            dust.loadSource(dust.compile("Hello, {name}!", "index"));
+            cb(null);
+        };
+
+        dust.render('index', { name: 'world' }, function (err, data) {
+            t.error(err);
+            t.equal(data, 'Hello, world!');
+            t.equal(typeof dust.cache.index, 'function');
+            t.equal(dust.load.name, 'cabbage');
+
+            dust.cache = {};
+            undo();
+
+            t.equal(dust.load.name, '');
+            t.end();
+        });
+    });
+
+
+    t.test('cache disabled', function (t) {
+        var undo = contextify({ cache: false });
+
+        t.plan(9);
+
+        dust.onLoad = function (name, context, cb) {
+            t.equals(name, 'index');
+            t.equals(typeof context, 'object');
+            t.equals(context.get('name'), 'world');
+            t.equals(typeof cb, 'function');
+
+            dust.loadSource(dust.compile("Hello, {name}!", "index"));
+            cb(null);
+        };
+
+        dust.render('index', { name: 'world' }, function (err, data) {
+            t.error(err);
+            t.equal(data, 'Hello, world!');
+            t.equal(typeof dust.cache.index, "undefined");
+            t.equal(dust.load.name, 'cabbage');
+
+            dust.cache = {};
+            undo();
+
+            t.equal(dust.load.name, '');
+            t.end();
+        });
+    });
+
+
+});
