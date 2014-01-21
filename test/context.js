a274d1daadf39954c2f39e76240a6ff20224dc7d
ertoth <ertoth@paypal.com>
2014-01-21 14:56:42 -0800

    more tests

--- test/context.js
+++ test/context.js
@@ -30,6 +30,33 @@ test('dustjs-onload-context', function (t) {
     });
 
 
+    t.test('patched without context', function (t) {
+        var undo = contextify();
+
+        t.plan(7);
+
+        dust.onLoad = function (name, cb) {
+            t.equals(name, 'index');
+            t.equals(typeof cb, 'function');
+            cb(null, 'Hello, {name}!');
+        };
+
+        dust.render('index', { name: 'world' }, function (err, data) {
+            t.error(err);
+            t.equal(data, 'Hello, world!');
+            t.equal(typeof dust.cache.index, 'function');
+            t.equal(dust.load.name, 'cabbage');
+
+            dust.cache = {};
+            undo();
+
+            t.equal(dust.load.name, '');
+            t.end();
+        });
+
+    });
+
+
     t.test('with context', function (t) {
         var undo = contextify();
 
@@ -59,7 +86,7 @@ test('dustjs-onload-context', function (t) {
     });
 
 
-    t.test('primed cache', function (t) {
+    t.test('prime cache on load', function (t) {
         var undo = contextify();
 
         t.plan(9);
@@ -122,16 +149,45 @@ test('dustjs-onload-context', function (t) {
     t.test('error', function (t) {
         var undo = contextify();
 
+        t.plan(5);
+
         dust.silenceErrors = true;
         dust.onLoad = function (name, context, cb) {
-            console.log('load');
+            t.ok(name);
+            t.ok(context);
+            t.ok(cb);
             cb(new Error('test'));
         };
 
         dust.render('index', { name: 'world' }, function (err, data) {
-            undo();
             t.ok(err);
             t.equal(data, undefined);
+
+            dust.cache = {};
+            undo();
+
+            t.end();
+        });
+    });
+
+
+    t.test('cached template', function (t) {
+        var undo = contextify();
+
+        t.plan(2);
+
+        dust.onLoad = function (name, context, cb) {
+            cb(new Error('Should not be called'));
+        };
+
+        dust.loadSource(dust.compile("Hello, {name}!", "index"));
+        dust.render('index', { name: 'world' }, function (err, data) {
+            t.error(err);
+            t.equal(data, 'Hello, world!');
+
+            dust.cache = {};
+            undo();
+
             t.end();
         });
     });
